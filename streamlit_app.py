5fabfc1f455e805209e11dff9faad4cd073296db
Darren McEwan <63265943+darrenmcewan@users.noreply.github.com>
2022-08-09 15:39:57 -0700

    Update streamlit_app.py
    Fixed password bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit as st
 import pandas as pd
 
 def check_password():
@@ -36,11 +36,11 @@ if check_password():
 fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_list = fruit_list.set_index('Fruit')
 
-streamlit.title("Darren's Fruit Shack")
+st.title("Darren's Fruit Shack")
 
-streamlit.header("Create Your Own Smoothie")
+st.header("Create Your Own Smoothie")
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = st.multiselect("Pick some fruits:", list(fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
