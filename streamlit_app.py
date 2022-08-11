6945a772affed628dffeff55f755d5747a0bbee9
Darren McEwan <63265943+darrenmcewan@users.noreply.github.com>
2022-08-11 15:39:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,14 +26,14 @@ def check_password():
         # Password correct.
         return True
 
- def displayPDF(file):
+def displayPDF(file):
     # Opening file from file path
     with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     # Embedding PDF in HTML
     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>' 
-    
+
     # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
 
