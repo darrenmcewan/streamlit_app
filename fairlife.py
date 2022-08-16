fcc6edaa77b204234f4260715177c7ca7bffdc41
Darren McEwan <darren@mcewan.me>
2022-08-16 16:57:11 -0700

    Update fairlife.py

--- fairlife.py
+++ fairlife.py
@@ -19,7 +19,7 @@ def send_email_gmail(subject, message, destination):
     msg['To'] = destination
     server.send_message(msg)
 
-def check(products, email):
+def check(products, email=""):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'} 
     fairlife_flavors = ['Chocolate','Vanilla','Salted Caramel','Strawberry']
     fairlife_links = ['https://www.samsclub.com/p/fairlife-nutrition-plan-chocolate-30-g-protein-shake-11-5-fl-oz-12-pk/prod24381709',
@@ -56,7 +56,6 @@ def check(products, email):
             send_email_gmail(email_subject, body_of_email, email)
             st.success(f'Email sent to {email}')
            
-    else:
-        st.error("Error sending email")
+
     
         
